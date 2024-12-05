486482ad9dd49f148557120effdae418926dab5a
stepit <stefanofrancesco.pitton@gmail.com>
2024-12-05 12:40:45 +0100

    refactor: improve lazy colors

--- lua/flow/highlights/lazy.lua
+++ lua/flow/highlights/lazy.lua
@@ -3,20 +3,26 @@ local M = {}
 -- Defines the highlight group colors for
 -- the lazy plugin manager.
 -- @param c The available colors.
-function M.get(c, options)
+function M.get(c, _)
   local theme = {
+    LazyH1 = { bg = c.fluo, fg = c.grey[2] },
+
     LazyProgressDone = { bold = true, fg = c.fluo },
     LazyProgressTodo = { bold = true, fg = c.light_blue },
 
-    LazyButtonActive = { bold = true, bg = c.fluo, fg = c.black },
     LazyButton = { bg = c.bg_highlight, fg = c.grey[7] },
     LazyNormal = { fg = c.light_blue },
-    LazyReasonRuntime = { fg = c.grey[4] },
+    LazyButtonActive = { bold = true, bg = c.fluo, fg = c.black },
 
     LazyReasonStart = { fg = c.purple },
     LazyReasonSource = { link = "LazyReasonStart" },
     LazyReasonPlugin = { link = "LazyReasonStart" },
+    LazyReasonRuntime = { fg = c.grey[4] },
+    LazyReasonEvent = { fg = c.red },
     LazyReasonCmd = { fg = c.sky_blue },
+
+    LazySpecial = { fg = c.yellow },
+    LazyDimmed = { fg = c.grey[7] },
   }
 
   return theme
