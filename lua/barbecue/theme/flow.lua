e8c0f9fc990411238af9fb91e4b6fe7ea48c5137
stepit <stefanofrancesco.pitton@gmail.com>
2025-01-15 11:18:15 +0100

    update colors

--- lua/barbecue/theme/flow.lua
+++ lua/barbecue/theme/flow.lua
@@ -4,24 +4,25 @@ local c = require("flow.colors").setup(options)
 local M = {
   normal = { bg = c.none, fg = c.fg_dark },
 
-  ellipsis = { fg = c.to_check },
+  ellipsis = { fg = c.fg_gutter },
   separator = { fg = c.fg_gutter },
   modified = { fg = c.yellow },
 
   dirname = { fg = c.comment },
-  basename = { fg = c.light_blue, bold = true },
-  context = { fg = c.fg_dark },
+  basename = { fg = c.light_blue },
+  context = { fg = c.grey[6] },
 
-  context_enum = { fg = c.orange },
-  context_interface = { fg = c.orange },
-  context_number = { fg = c.orange },
-  context_boolean = { fg = c.orange },
-  context_array = { fg = c.orange },
-  context_event = { fg = c.orange },
   context_object = { fg = c.cyan },
-  context_class = { link = "context_object" },
-  context_struct = { link = "context_object" },
-  context_file = { fg = c.to_check },
+  context_class = { fg = c.cyan },
+  context_struct = { fg = c.cyan },
+
+  context_enum = { fg = c.purple },
+  context_interface = { fg = c.purple },
+  context_array = { fg = c.purple },
+  context_event = { fg = c.yellow },
+  context_number = { fg = c.yellow },
+  context_boolean = { link = "Number" },
+  context_file = { link = "Directory" },
   context_module = { fg = c.yellow },
   context_namespace = { fg = c.yellow },
   context_package = { fg = c.blue },
@@ -32,12 +33,12 @@ local M = {
   context_function = { fg = c.blue },
   context_variable = { fg = c.purple },
   context_constant = { fg = c.purple },
-  context_string = { fg = c.green },
+  context_string = { link = "String" },
   context_key = { fg = c.purple },
   context_null = { fg = c.blue },
   context_enum_member = { fg = c.green },
   context_operator = { fg = c.green },
-  context_type_parameter = { fg = c.green },
+  context_type_parameter = { fg = c.purple },
 }
 
 return M
