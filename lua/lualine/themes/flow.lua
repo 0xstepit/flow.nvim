8edcad62abb95cf1c7b33be34fe184686f671a6e
stepit <stefanofrancesco.pitton@gmail.com>
2025-01-15 21:18:42 +0100

    update lualine colors

--- lua/lualine/themes/flow.lua
+++ lua/lualine/themes/flow.lua
@@ -5,44 +5,44 @@ local flow = {}
 -- Normal mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.normal = {
-  a = { bg = colors.light_blue, fg = colors.grey[4] },
-  b = { bg = colors.grey[4], fg = colors.light_blue },
+  a = { bg = colors.light_blue, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.light_blue },
   c = { bg = colors.bg_statusline, fg = colors.fg_statusline },
 }
 
 -- Insert mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.insert = {
-  a = { bg = colors.cyan, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.cyan },
+  a = { bg = colors.cyan, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.cyan },
 }
 
 -- Command mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.command = {
-  a = { bg = colors.yellow, fg = colors.fg_gutter },
-  b = { bg = colors.fg_gutter, fg = colors.yellow },
+  a = { bg = colors.yellow, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.yellow },
 }
 
 -- Visual mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.visual = {
-  a = { bg = colors.purple, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.purple },
+  a = { bg = colors.purple, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.purple },
 }
 
 -- Replace mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.replace = {
-  a = { bg = colors.red, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.red },
+  a = { bg = colors.red, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.red },
 }
 
 -- Terminal mode colors. a will be used also for x,
 -- b for y, and c for z to have a symmetrical coloration.
 flow.terminal = {
-  a = { bg = colors.green, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.green },
+  a = { bg = colors.green, fg = colors.grey[2] },
+  b = { bg = colors.grey[2], fg = colors.green },
 }
 
 -- Inactive mode colors. Only c seems to be applied here
