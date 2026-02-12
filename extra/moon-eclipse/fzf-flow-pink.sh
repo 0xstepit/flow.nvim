a3245554c2c8ba73a4ecf5832e18a389bb6bee3d
stepit <stefanofrancesco.pitton@gmail.com>
2026-02-12 11:44:43 +0100

    chore: make extra

--- extra/moon-eclipse/fzf-flow-pink.sh
+++ extra/moon-eclipse/fzf-flow-pink.sh
@@ -6,10 +6,10 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
 --height 40% \
 --layout=reverse \
 --color=bg+:#1b2328 \
---color=fg:#d1dbe0 \
---color=fg+:#d1dbe0 \
---color=hl:#3d505c \
---color=hl+:#3d505c \
+--color=fg:#94aab8 \
+--color=fg+:#94aab8 \
+--color=hl:#668599 \
+--color=hl+:#668599 \
 --color=border:#1f282e \
 --color=info:#70afdb \
 --color=marker:#a670db \
