76865f14310467724bfdc7730e8f7671ef6c75b4
stepit <stefanofrancesco.pitton@gmail.com>
2026-03-13 18:54:56 +0100

    chore: render md hi cleanup

--- lua/flow/highlights/render-markdown.lua
+++ lua/flow/highlights/render-markdown.lua
@@ -1,47 +1,36 @@
 local M = {}
 
---- @param c table: The available colors.
---- @param o FlowConfig: The available options.
---- @return table: Render markdown plugin highlights.
-function M.get(c, o)
+--- @param colors table The available colors.
+--- @param config FlowConfig The available options.
+--- @return table Render markdown plugin highlights.
+function M.get(colors, config)
   local theme = {
-    RenderMarkdownCode = { link = "@markup.raw.block" }, -- Code blocks.
-    RenderMarkdownCodeInline = { link = "@markup.raw.markdown_inline" }, -- Code inline.
+    RenderMarkdownCode = { link = "@markup.raw.block" },
+    RenderMarkdownCodeInline = { link = "@markup.raw.markdown_inline" },
     RenderMarkdownBullet = { link = "@markup.list" },
     RenderMarkdownDash = { link = "Comment" },
-
-    -- TODO: complete
-    -- | RenderMarkdownTableHead       | @markup.heading                    | Pipe table heading rows    |
-    -- | RenderMarkdownTableRow        | Normal                             | Pipe table body rows       |
-    -- | RenderMarkdownTableFill       | Conceal                            | Pipe table inline padding  |
-    -- | RenderMarkdownSuccess         | DiagnosticOk                       | Success related callouts   |
-    -- | RenderMarkdownInfo            | DiagnosticInfo                     | Info related callouts      |
-    -- | RenderMarkdownHint            | DiagnosticHint                     | Hint related callouts      |
-    -- | RenderMarkdownWarn            | DiagnosticWarn                     | Warning related callouts   |
-    -- | RenderMarkdownError           | DiagnosticError                    | Error related callouts     |
   }
 
   -- Sequentially define headers background colors.
   local headersBg = {
-    c.Fluo.light,
-    c.Blue.very_light,
-    c.Light_blue.very_light,
-    c.Cyan.very_light,
-    c.Sky_blue.very_light,
+    colors.Fluo.light,
+    colors.Blue.very_light,
+    colors.Light_blue.very_light,
+    colors.Cyan.very_light,
+    colors.Sky_blue.very_light,
   }
-  if o.theme.style == "dark" then
+  if config.theme.style == "dark" then
     headersBg = {
-      c.Fluo.dark,
-      c.Blue.very_dark,
-      c.Light_blue.very_dark,
-      c.Cyan.very_dark,
-      c.Sky_blue.very_dark,
+      colors.Fluo.dark,
+      colors.Blue.very_dark,
+      colors.Light_blue.very_dark,
+      colors.Cyan.very_dark,
+      colors.Sky_blue.very_dark,
     }
   end
 
-  -- TODO: hardcoded transparent background but should be based on config.
   for i, _ in ipairs(headersBg) do
-    theme["RenderMarkdownH" .. i .. "Bg"] = { bg = c.transparent, bold = true }
+    theme["RenderMarkdownH" .. i .. "Bg"] = { bg = colors.transparent, bold = true }
   end
 
   return theme
